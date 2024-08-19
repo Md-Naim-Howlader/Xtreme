f57b2966864644cfece7f23c6aa4323fa55fd41e
Md-Naim-Howlader <mdnaimptk776@gmail.com>
2024-08-19 22:29:06 +0600

    done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        primary: "#008374",
+        secondry: "#F6F6F6",
+      },
+      colors: {
+        primary: "#008374",
+        danger: "#f96f59",
+        secondry: "#fff9",
+      },
+    },
+  },
+  plugins: [],
+};
